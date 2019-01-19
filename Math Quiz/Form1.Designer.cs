96146a3efa09d0122c3281a7e66a92a00b8aefcd
Jeremy Cherriman <32113470+JNCherriman@users.noreply.github.com>
2019-01-19 09:57:29 -0500

    To create the time remaining box Add a Label control from the Toolbox, and then set the value of its (Name) property to timeLabel.
    This label will become a box in the upper-right corner that shows the number of seconds that remain in the quiz.
    
    Change the AutoSize property to False so that you can resize the box.
    
    Change the BorderStyle property to FixedSingle to draw a line around the box.
    
    Set the Size property to 200, 30.
    
    Move the label to the upper-right corner of the form, where blue spacer lines will appear.
    
    These lines help you align controls on the form.
    
    In the Properties window, choose the Text property, and then choose the Backspace key to clear its value.
    
    Choose the plus sign (+) next to the Font property, and then change the value of the Size property to 15.75.
    
    You can change several font properties, as the following picture shows.
    
    Properties window showing font size
    
    Add another Label control from the Toolbox, and then set its font size to 15.75.
    
    Set the Text property to Time Left.
    
    Move the label so that it lines up just to the left of the timeLabel label.

--- Math Quiz/Form1.Designer.cs
+++ Math Quiz/Form1.Designer.cs
@@ -28,22 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.timeLeftLable = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Location = new System.Drawing.Point(272, 9);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(200, 30);
+            this.timeLabel.TabIndex = 0;
+            // 
+            // timeLeftLable
+            // 
+            this.timeLeftLable.AutoSize = true;
+            this.timeLeftLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLeftLable.Location = new System.Drawing.Point(165, 10);
+            this.timeLeftLable.Name = "timeLeftLable";
+            this.timeLeftLable.Size = new System.Drawing.Size(101, 25);
+            this.timeLeftLable.TabIndex = 1;
+            this.timeLeftLable.Text = "Time Left";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.timeLeftLable);
+            this.Controls.Add(this.timeLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Jeremy Cherriman - Math Quiz";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label timeLeftLable;
     }
 }
 
